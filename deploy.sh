964958a23fb57cb9cd9a945e48624d29ac4b929f
Aaron Kearns <x20513033@student.ncirl.ie>
2024-04-12 00:45:03 +0100

    config.yml edit, deploy.sh edit

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,4 @@ sudo npm install -g pm2
 pm2 stop devopsproject
 cd DevOpsProject/
 npm install
-echo $PRIVATE_KEY > privatekey.pem
-echo $SERVER > server.crt
 pm2 start ./bin/www --name devopsproject
